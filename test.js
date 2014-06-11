f30d2acae9d5e61d4cc4a53c776949b72a56340a
Marshall Rose <mrose17@gmail.com>
2014-06-11 10:51:33 -0400

    0.4.2

--- test.js
+++ test.js
@@ -1,10 +1,11 @@
-    var arp = require('./index')
-      , tbl = { ipaddrs: {}, ifnames : {} }
+    var arp  = require('./index')
+      , tbl  = { ipaddrs: {}, ifnames : {} }
+      , util = require('util')
       ;
 
     arp.arpTable(function(err, entry) {
       if (err) console.log('arp: ' + err.message);
-      if (!entry) return console.log(JSON.stringify(tbl));
+      if (!entry) return console.log(util.inspect(tbl, { depth: null }));
 
       tbl.ipaddrs[entry.ip] = { ifname : entry.ifname, mac: entry.mac };
       if (!tbl.ifnames[entry.ifname]) tbl.ifnames[entry.ifname] = {};
@@ -14,5 +15,5 @@
     arp.ifTable(function(err, entry) {
       if (err) return console.log('arp: ' + err.message);
 
-      if (!!entry) console.log(entry);
+      if (!!entry)  return console.log(util.inspect(entry, { depth: null }));
     });
