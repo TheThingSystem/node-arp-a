02dae068c0e3db1c0e1fb356df0087e3cbda1267
stickpin <630000+stickpin@users.noreply.github.com>
2019-12-13 14:37:32 +0100

    Flags support for Linux

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ if (process.platform.indexOf('linux') === 0) {
 
         cols = lines[i].replace(/ [ ]*/g, ' ').split(' ');
         if ((cols.length > 3) && (cols[0].length !== 0) && (cols[3].length !== 0)) {
-          cb(null, { ip: cols[0], mac: cols[3], iface: cols[5] });
+          cb(null, { ip: cols[0], flag: cols[2], mac: cols[3], iface: cols[5] });
         }
       }
 
